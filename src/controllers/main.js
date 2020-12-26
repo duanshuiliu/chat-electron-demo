1475ac1653c1caef15ae91a061fc3eb0a5998dab
kelvin <kelvin_512@sina.com>
2020-12-26 12:20:11 +0800

    聊天框个人信息显示优化

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -1,9 +1,10 @@
 let uid      = ipcRenderer.sendSync("session", "uid");
 let username = ipcRenderer.sendSync("session", "username");
 
-// $(".kl-kelvin").MyChattool({
-//     userId: uid,
-//     username: username,
-// });
+$(".kl-kelvin").MyChattool({
+    userId: uid,
+    username: username,
+    websocketUrl: 'ws://192.168.1.14:13000/chat',
+});
 
-// $(".kl-body-user .header .header-title").text(username);
+$(".kl-body-user .header .info-name").text(username);
