3073e8de13bf36f49fcf2c3e26dd472cb0ce926c
kelvin <kelvin_512@sina.com>
2020-12-26 12:13:20 +0800

    个人信息显示调整

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -1,9 +1,9 @@
 let uid      = ipcRenderer.sendSync("session", "uid");
 let username = ipcRenderer.sendSync("session", "username");
 
-$(".kl-kelvin").MyChattool({
-    userId: uid,
-    username: username,
-});
+// $(".kl-kelvin").MyChattool({
+//     userId: uid,
+//     username: username,
+// });
 
-$(".kl-body-user .header .header-title").text(username);
+// $(".kl-body-user .header .header-title").text(username);
